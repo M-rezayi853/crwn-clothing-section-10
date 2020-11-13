56b8e62b7967fd2b73f897b21266222ebdc6a21f
mahdi <mahdi.rezayi.853@gmail.com>
2020-11-13 13:55:37 +0330

    translate directory state and collection state into redux, builded collection-overview component

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
+
+
+const Shop = () => {
+
+    return (
+        <div className="shop-page">
+            <CollectionsOverview />
+        </div>
+    );
+};
+
+
+export default Shop;
